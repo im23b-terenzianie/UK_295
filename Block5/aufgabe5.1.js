2c38d91fe9ae4816b7eb3fb812e710f41ec2ae87
im23b-terenzianie <terenzianie@bzz.ch>
2025-04-15 13:01:26 +0200

    Aufgabe5.2 erledigt

--- Block5/aufgabe5.1.js
+++ Block5/aufgabe5.1.js
@@ -9,6 +9,29 @@ const books = [
     { isbn: "9783825530176", title: "Die unendliche Geschichte", author: "Michael Ende" },
     { isbn: "9783608950123", title: "Effi Briest", author: "Theodor Fontane" }
   ]
+  const lends = [
+    {
+      id: 1,
+      customer_id: "K1001",
+      isbn: "9783161484100",
+      borrowed_at: "2025-04-15T12:00:00Z",
+      returned_at: null
+    },
+    {
+      id: 2,
+      customer_id: "K1002",
+      isbn: "9783825530176",
+      borrowed_at: "2025-04-10T14:30:00Z",
+      returned_at: "2025-04-12T16:45:00Z"
+    },
+    {
+      id: 3,
+      customer_id: "K1003",
+      isbn: "9783608950123",
+      borrowed_at: "2025-04-14T09:15:00Z",
+      returned_at: null
+    }
+  ];
 app.get('/books',(req,res)=>{
     res.send(books)
 
@@ -56,8 +79,31 @@ app.patch('/books/:isbn',(req,res)=>{
     })
     res.send(books);
 })
-
-
+app.get('/lends',(req, res)=>{
+    res.send(lends)
+});
+app.get('/lends/:id',(req,res)=>{
+    const id = Number(req.params.id);
+    const lend = lends.find(b=> b.id === id)
+    res.send(lend)
+});
+app.post('/lends',(req,res)=>{
+  const {customer_id, isbn} = req.body;
+  const newId = lends.length > 0 ? Math.max(...lends.map(lend => lend.id)) + 1 : 1;
+  const newlend ={
+    id: newId,
+    customer_id: customer_id.trim(),
+    isbn: isbn.trim(),
+    borrowed_at: new Date().toISOString(),
+  }
+  lends.push(newLend);
+  res.send(newlend)
+})
+app.delete('/lend/:id',(req,res)=>{
+  const id = Number(req.params.id);
+  const index = lends.findIndex(lend => lend.id === id);
+  lends.splice(index,1)
+});
 app.listen(port, (req,res) => {
     console.log(`Example app listening on port ${port}`);
   });
